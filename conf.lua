c170be79570b5f9947d9542458c653beb169a044
Noah <noah.buergler@gmail.com>
2023-06-12 20:24:39 +0200

    Restructure and Fixed Fullscreen Bug

--- conf.lua
+++ conf.lua
@@ -1,4 +1,6 @@
-local version = "1.9.6"
+local settings = require('settings')
+local version = settings.version
+local res = settings.resolution or 'HD'
 
 function love.conf(t)
     t.title = "L.C.S. - Version "..version.." - Von Noah, David und Samuel"
@@ -6,12 +8,18 @@ function love.conf(t)
     t.console = true
     t.identity = "LCS_SAVE_2"
     
-    -- t.window.width, t.window.height = 1280, 720
-    t.window.width, t.window.height = 1920, 1080
+    local resolutions = {
+        HD  = {1280,  720},
+        FHD = {1920, 1080},
+        QHD = {2560, 1440},
+        UHD = {3840, 2160}
+    }
+    t.window.width, t.window.height = unpack(resolutions[res])
     t.window.resizable = true
     t.window.msaa = 8
     t.window.vsync = false
     t.window.usedpiscale = false
+    t.window.borderless = false
 
     t.modules.audio = false
     t.modules.joystick = false
---
+++ settings.lua
@@ -0,0 +1,14 @@
+local settings = {}
+settings.version = '1.9.6'
+settings.savefolder = 'LCS_SAVE_2'
+--[[
+    Availlable Resolutions (Default is HD):
+    HD / 720p
+    FHD / 1080p
+    QHD / WQHD / 1440p
+    UHD / 4K / 2160p
+]]
+settings.resolution = 'FHD'
+
+
+return settings
